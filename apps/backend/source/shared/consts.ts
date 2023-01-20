682e004ecd15206404b93f2dd6a209ad4d6d3a2b
ShahanaFarooqui <shahana.farooqui@gmail.com>
2023-01-20 12:16:55 -0800

    adding testnet config to git ignore

--- apps/backend/source/shared/consts.ts
+++ apps/backend/source/shared/consts.ts
@@ -1,3 +1,5 @@
+import * as crypto from 'crypto';
+
 export enum Environment {
   PRODUCTION = 'production',
   TESTING = 'testing',
@@ -7,13 +9,6 @@ export enum Environment {
 export const API_VERSION = '/v1';
 export const NODE_ENV: Environment = Environment.DEVELOPMENT;
 export const LOG_FILE = './dist/logs/cln-backend-' + new Date().toISOString() + '.log';
-export const COMMANDO_RUNE = 'Am3W_wI0PRn4qVNEsJ2iInHyFPQK8wfdqEXztm8-icQ9MA==';
-export const COMMANDO_PUBKEY = '0233263e392b38d0d905e8b35cead00063e8a6b7601d2d2bc3b97348e39026178c';
-export const COMMANDO_WS_PROXY = 'ws://0.0.0.0:5001';
-export const COMMANDO_IP = '0.0.0.0';
-export const COMMANDO_PORT = 19846;
-export const COMMANDO_PRIVATE_KEY =
-  'ea8d3091934f2c86c216370f0206acaaa2ee12462387743c358ca5f0245bf561';
 
 export enum HttpStatusCode {
   GET_OK = 200,
