1110d29a201ad67c43d53d2d2fba94bbc1b00388
ShahanaFarooqui <shahana.farooqui@gmail.com>
2023-03-07 21:49:08 -0800

    Config location fix for app on docker

--- exports.sh
+++ exports.sh
@@ -23,7 +23,7 @@ export APP_CORE_LIGHTNING_TOR_HOST=""
 
 export APP_CORE_LIGHTNING_RUNE="NZ0wOuBtKlKdmQ_MBsME6Hlbo0kKYNNBo8gUZFYuFpI9MA=="
 export APP_CORE_LIGHTNING_NODE_PUBKEY="034c46fcb1a954ca34f61a3248c1854ff7f97b27217ffa9c9caafa81f78c772894"
-export APP_CORE_LIGHTNING_CONFIG_LOCATION="/data/app/config.json"
+export APP_CORE_LIGHTNING_CONFIG_LOCATION="../../../../../data/app/config.json"
 export APP_CORE_LIGHTNING_LOG_FILE_LOCATION="/app/apps/backend/dist/log/cln.log"
 
 # export APP_CORE_LIGHTNING_DAEMON_IP="localhost"
